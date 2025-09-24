9024a6fb3a8bc8236120923014d4bc28d7509797
mz038197 <mz038197@gmail.com>
2025-09-24 23:25:23 +0800

    update

--- utils/lab_utils_uni.py
+++ utils/lab_utils_uni.py
@@ -8,9 +8,9 @@ from matplotlib.ticker import MaxNLocator
 from matplotlib.gridspec import GridSpec
 from matplotlib.colors import LinearSegmentedColormap
 from ipywidgets import interact
-from utils.lab_utils_common import compute_cost
-from utils.lab_utils_common import dlblue, dlorange, dldarkred, dlmagenta, dlpurple, dlcolors
-plt.style.use('../utils/deeplearning.mplstyle')
+from lab_utils_common import compute_cost
+from lab_utils_common import dlblue, dlorange, dldarkred, dlmagenta, dlpurple, dlcolors
+#plt.style.use('../utils/deeplearning.mplstyle')
 
 n_bin = 5
 dlcm = LinearSegmentedColormap.from_list(
