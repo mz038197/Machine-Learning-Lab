ba60124310ffb9bbfc870cea47482c3805c7dab8
mz038197 <mz038197@gmail.com>
2025-09-25 23:46:14 +0800

    update

--- lab/nbgrader_config.py
+++ lab/nbgrader_config.py
@@ -8,7 +8,7 @@ c = get_config()
 # server set up.
 c.CourseDirectory.course_id = "lab"
 
-c.IncludeHeaderFooter.header = "source/header.ipynb"
+#c.IncludeHeaderFooter.header = "source/header.ipynb"
 
 ###############################################################################
 # End additions by nbgrader quickstart
